43a3e3e23cf8b33695fe7500c4249eb47796b3b8
KimGyeongyeon <kgy556@kaist.ac.kr>
2020-08-01 15:19:05 +0900

    simple tab and head img

--- src/App.js
+++ src/App.js
@@ -2,25 +2,83 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Tabs extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      active: 0
+    }
+  }
+  
+  select = (i) => {
+    let _this = this;
+    return function() {
+      _this.setState({
+        active: i
+      });
+    }
+  }
+  
+  renderTabs = () => {
+    return React.Children.map(this.props.children, (item, i) => {
+      if (i%2 === 0) {
+        let active = this.state.active === i ? 'active' : '';
+        return <a onClick={this.select(i)} className={`${active} tab`}>{item}</a>;
+      }
+    });
+  }
+  
+  renderContent() {
+    return React.Children.map(this.props.children, (item, i) => {
+      if (i-1 === this.state.active) {
+        return <div className='content'>{item}</div>;
+      } else {
+        return;
+      }
+    });
+  }
+  
+  render() {
+    return (
+      <div className="tabs">
+        {this.renderTabs()}
+        {this.renderContent()}
+      </div>
+    );
+  }
 }
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <div id = "header"  >
+          <h1>
+            <img src = 'https://www.queen.co.kr/news/photo/201904/310834_49716_1349.jpg' 
+            alt = 'logo'/>
+          </h1>
+        </div>
+        <Tabs>
+          One
+          <span>
+            Well here's a nested Tabs element.
+            <Tabs>
+              One
+              <span>One</span>
+              Two
+              <span>Two</span>
+            </Tabs>
+          </span>
+          Two
+          <span>Two thing</span>
+          Three
+          <span>Three thing</span>
+        </Tabs>
+      </main>
+    );
+  }
+}
+
+//React.render(<App/>, document.getElementById('app'));
 
 export default App;
